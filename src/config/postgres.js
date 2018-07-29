379b3908057bd18129efcebf1590f06cdbcdd49f
jmedinac1987 <jmedinac1987@gmail.com>
2018-07-29 16:21:06 -0500

    Ajuste de variable de entorno para conexion a postgres

--- src/config/postgres.js
+++ src/config/postgres.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const postgres = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://postgres:jorge@localhost/mevn',
+  connectionString: process.env.DATABASE_URL,
   // maximum number of clients the pool should contain
   max: 60,
   ssl: true  
