b44d74389ef97de835fb85957ffa580f831c29f3
jmedinac1987 <jmedinac1987@gmail.com>
2018-07-29 16:08:02 -0500

    Ajuste de ssl para la conexion postgres

--- src/config/postgres.js
+++ src/config/postgres.js
@@ -2,7 +2,8 @@ const { Pool } = require('pg');
 const postgres = new Pool({
   connectionString: process.env.DATABASE_URL || 'postgres://postgres:jorge@localhost/mevn',
   // maximum number of clients the pool should contain
-  max: 60  
+  max: 60,
+  ssl: true  
 });
 
  module.exports = postgres;
